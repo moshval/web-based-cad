a05efbad808c8ed65781799dff7c7364af5a4699
rahmannnnn <arifingatrahman@gmail.com>
2022-02-27 18:40:27 +0700

    refactor

--- src/line.js
+++ src/line.js
@@ -14,14 +14,12 @@ lineButton.addEventListener('click',function(){
 })
 
 function drawLine(e){ // Draw Line by locating vertex position
-    if(drawingMode == true && draggingMode ==false && drawingLine == true && movingMode == false){
+    if(drawingMode == true && draggingMode == false && drawingLine == true && movingMode == false){
         let xCoor = e.clientX - canvas.getBoundingClientRect().left;
         let yCoor = e.clientY - canvas.getBoundingClientRect().top;
         let col = vec4(red,green,blue,1.0);
         tempCol.push(col);
-        console.log(xCoor,yCoor);
         let coord = coordinateCreator(xCoor,yCoor);
-        console.log(coord.X,coord.Y);
         polyVertices.push(coord);
         
         // If clicked line points == 2
@@ -38,19 +36,19 @@ function drawLine(e){ // Draw Line by locating vertex position
             
             // Color included
             for (let index = 0; index < polyVertices.length; index++) {
-                gl.bindBuffer(gl.ARRAY_BUFFER,color_buffer);
-                gl.bufferSubData(gl.ARRAY_BUFFER,16*vertIndex,flatten(tempCol[polyVertices.length-1])); //only uses last color picked by a shape (bisa diubah ke per vertex kalo mau)
+                gl.bindBuffer(gl.ARRAY_BUFFER, color_buffer);
+                gl.bufferSubData(gl.ARRAY_BUFFER, 16*vertIndex, flatten(tempCol[polyVertices.length-1])); //only uses last color picked by a shape (bisa diubah ke per vertex kalo mau)
                 line.vertIdx.push(vertIndex);
                 line.vertices.push(polyVertices[index]);
                 vertIndex++;    
             }
+
             vertIndex++;
             vertIndex++;
             tempCol = [];
             id++;
             shapeData.push(line);
             objectDrawer(shapeData);
-
         }
 
     }
